5f125041de07c0ac909fce2110aedf7bdedef7ab
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-09-13 18:09:43 +0900

    Fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,6 @@
-#! /bin/bash
+#!/bin/bash
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 set -e
 IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
@@ -20,6 +22,12 @@ else
     uprefix="$PREFIX"
 fi
 
+# Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
+# process of excising Libtool files from our packages. Existing ones can break
+# the build while this happens. We have "/." at the end of $uprefix to be safe
+# in case the variable is empty.
+find $uprefix/. -name '*.la' -delete
+
 # On Windows we need to regenerate the configure scripts.
 if [ -n "$CYGWIN_PREFIX" ] ; then
     am_version=1.15 # keep sync'ed with meta.yaml
@@ -32,17 +40,32 @@ if [ -n "$CYGWIN_PREFIX" ] ; then
         -I "$BUILD_PREFIX_M/Library/mingw-w64/share/aclocal"
     )
     autoreconf "${autoreconf_args[@]}"
+
+    # And we need to add the search path that lets libtool find the
+    # msys2 stub libraries for ws2_32.
+    platlibs=$(cd $(dirname $(gcc --print-prog-name=ld))/../lib && pwd -W)
+    export LDFLAGS="$LDFLAGS -L$platlibs"
 fi
 
 export PKG_CONFIG_LIBDIR=$uprefix/lib/pkgconfig:$uprefix/share/pkgconfig
 configure_args=(
     --prefix=$mprefix
+    --disable-static
     --disable-dependency-tracking
+    --disable-selective-werror
     --disable-silent-rules
 )
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]] ; then
+    configure_args+=(
+        --enable-malloc0returnsnull
+    )
+fi
 ./configure "${configure_args[@]}"
 make -j$CPU_COUNT
 make install
-make check
+rm -rf $uprefix/share/man $uprefix/share/doc/${PKG_NAME#xorg-}
 
-# remove all docs
+# Remove any new Libtool files we may have installed. It is intended that
+# conda-build will eventually do this automatically.
+find $uprefix/. -name '*.la' -delete
